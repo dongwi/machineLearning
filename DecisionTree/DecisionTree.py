01a74836953ddd20f3a43a3d6644979723c7e842
dongwi <52236474@qq.com>
2019-03-13 00:24:29 +0800

    【feat】【提交决策树基础实现】

---
+++ DecisionTree/DecisionTree.py
@@ -0,0 +1,19 @@
+from sklearn import datasets
+import numpy as np
+
+
+class DecisionTree:
+    def __init__(self):
+        return
+
+    def _calc_shannon(self):
+        return
+
+    def fit(self, X, y):
+        return
+
+
+if __name__=="__main__":
+    X, y = datasets.load_iris(True)
+    dt = DecisionTree()
+    dt.fit(X, y)
