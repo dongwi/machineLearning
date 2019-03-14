d5a4ae82a3392c9c04802d7dd391c664ce75b062
dongwi <52236474@qq.com>
2019-03-14 19:32:18 +0800

    【feat】【增加决策树部分实现】

--- DecisionTree/DecisionTree.py
+++ DecisionTree/DecisionTree.py
@@ -1,19 +1,38 @@
 from sklearn import datasets
 import numpy as np
+import pandas as pd
 
 
 class DecisionTree:
     def __init__(self):
         return
 
-    def _calc_shannon(self):
+    def _calc_shannon_ent(self, p):
+        return - (p * np.exp(p))
+
+    # 计算指定数据集的信息熵
+    def _calc_shannon_en(self, new_datasets):
+        n = len(new_datasets)
+        y = new_datasets.get('target')
+        label = np.unique(y)
+        ent = 0.0
+        for i in label:
+            ni = len(new_datasets[y==i])
+            ent += self._calc_shannon_ent(ni)
+        return ent
+
+    def _split(self):
         return
 
-    def fit(self, X, y):
+    def fit(self, dataset):
+        self.dataset = dataset
+        X = dataset.get('data')
+        y = dataset.get('target')
+        self._calc_shannon_en(dataset)
         return
 
 
 if __name__=="__main__":
-    X, y = datasets.load_iris(True)
+    dataset = datasets.load_iris()
     dt = DecisionTree()
-    dt.fit(X, y)
+    dt.fit(dataset)
